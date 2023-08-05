e76983e99ad1c2e142b5c37ed6525a21ce6b87ca
Szymon Jasinski <szjasinski@outlook.com>
2023-08-05 13:05:25 +0200

    adds the function of getting all the data for one judgement.

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-
-
-# driver = webdriver.Chrome(executable_path="/Users/szymon/PycharmProjects/Judgements/chromedriver")
-# driver.get("http://www.python.org")
-# assert "Python" in driver.title
-# elem = driver.find_element(By.NAME, "q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
-
-
 from selenium.webdriver import Chrome, ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
@@ -19,9 +6,50 @@ from selenium.webdriver.common.keys import Keys
 
 
 
+def get_wyrok_dict(driver):
+    tytuł = driver.find_element(By.CLASS_NAME, "war_header").text
+    orzeczenie = {"nazwa": tytuł}
+
+    keys = driver.find_elements(By.CLASS_NAME, "lista-label")
+    keys = [i.text for i in keys]
+
+    values = driver.find_elements(By.CLASS_NAME, "info-list-value")
+    values = [i.text for i in values]
+
+    reszta = driver.find_elements(By.CLASS_NAME, "info-list-value-uzasadnienie")
+    sentencja_value = reszta[0].text
+    if 'UZASADNIENIE' in keys:
+        uzasadnienie_value = reszta[1].text
+
+    for key, value in zip(keys, values):
+        orzeczenie[key] = value
+
+    orzeczenie['SENTENCJA'] = sentencja_value
+    orzeczenie['UZASADNIENIE'] = uzasadnienie_value
+
+    print(orzeczenie)
+    return orzeczenie
 
 
+def get_wyrok_links_list(driver):
 
+    info_lists = driver.find_elements(By.CLASS_NAME, "info-list-value")
+    wyroki = [info_lists[i] for i in range(0, len(info_lists), 2)]
+    linki = [wyroki[i].find_element(By.PARTIAL_LINK_TEXT, 'Wyrok') for i in range(len(wyroki))]
+
+    return linki
+
+
+def go_to_wyroks_page(driver):
+    elem = driver.find_element(By.ID, "hasla")
+
+    x = "Podatek od czynności cywilnoprawnych!"
+    elem.send_keys(x)
+
+    # szukaj = driver.find_element(By.XPATH, "//option[@value='Szukaj']")
+
+    szukaj = driver.find_element(By.XPATH, "//input[@type='submit']")
+    szukaj.click()
 
 def browser_function():
     options = ChromeOptions()
@@ -33,41 +61,20 @@ def browser_function():
     url = "https://orzeczenia.nsa.gov.pl/cbo/query"
     driver.get(url)
 
-    # window_before = driver.window_handles[0]
+    # database is a list of dictionaries, each containing data of a single judgement
+    database = []
 
-    elem = driver.find_element(By.ID, "hasla")
-
-    x = "Podatek od czynności cywilnoprawnych!"
-    elem.send_keys(x)
-
-    # szukaj = driver.find_element(By.XPATH, "//option[@value='Szukaj']")
-
-    szukaj = driver.find_element(By.XPATH, "//input[@type='submit']")
-    szukaj.click()
-
-    # window_after = driver.window_handles[1]
-
-    info_lists = driver.find_elements(By.CLASS_NAME, "info-list-value")
-    wyroki = [info_lists[i] for i in range(0, len(info_lists), 2)]
-    linki = [wyroki[i].find_element(By.PARTIAL_LINK_TEXT, 'Wyrok') for i in range(len(wyroki))]
-
-    # for i in linki:
-    #     print(i.text)
-
-    linki[0].click()
-
-    elem = driver.find_element(By.CLASS_NAME, "war_header")
-    print(elem.text)
-
-    z = "lista-label"
-    w = "info-list-value"
-
-
-
-
-    # driver.switch_to.window(window_after)
+    go_to_wyroks_page(driver)
+    links = get_wyrok_links_list(driver)
+    for link in links:
+        link.click()
+        judgement_data = get_wyrok_dict(driver)
+        database.append(judgement_data)
+        driver.back()
 
 
     # driver.quit()
 
-browser_function()
+
+if __name__ == "__main__":
+    browser_function()
