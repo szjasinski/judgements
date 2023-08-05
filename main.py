d9cf98983e25b76f0887929c923b63ff1fa88d37
Szymon Jasinski <szjasinski@outlook.com>
2023-08-05 18:23:40 +0200

    adds next page by url functionality

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
+
 
 from pages.main_page import MainPage
 
@@ -22,20 +24,38 @@ def browser_function():
     main_page.hasla_tematyczne_input.input_text("Podatek od czynności cywilnoprawnych!")
     main_page.szukaj_button.click()
 
-    # on results page
-    links = main_page.links_list
-    loop = range(len(links))
+    # tic = time.perf_counter()
 
-    for i in loop:
-        links[i].click()
-        judgement_data = main_page.details_dict
-        database.append(judgement_data)
-        driver.back()
+    result_pages_num = 3
+    for n in range(result_pages_num):
+        # on results page
         links = main_page.links_list
+        loop = range(len(links))
 
+        for i in loop:
+            links[i].click()
+            judgement_data = main_page.details_dict
+            database.append(judgement_data)
+            driver.back()
+            links = main_page.links_list
+
+        print("len database: ", str(len(database)))
+        for x in database:
+            print("Strona: ", str(n), ". Wyrok: ", x["nazwa"])
+
+        new_url = "https://orzeczenia.nsa.gov.pl/cbo/find?p=" + str(n+2)
+        main_page.set_url(new_url)
+        main_page.go()
+
+        # object for number of pages
+
+        # tac = time.perf_counter()
+        # print(tac-tic)
+
+    print('udalo sie!')
     print(len(database))
-    for x in database:
-        print(x["nazwa"], x['Data orzeczenia'], x['Sędziowie'])
+    print(len(set([database[i]['nazwa'] for i in range(len(database))])))
+
 
     # driver.quit()
 
