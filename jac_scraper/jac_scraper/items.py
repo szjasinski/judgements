78cc7fb3cb038f3bbfc8b8fcce7bcfff66836308
Szymon Jasinski <szjasinski@outlook.com>
2023-08-13 23:01:13 +0200

    adds better locators

--- jac_scraper/jac_scraper/items.py
+++ jac_scraper/jac_scraper/items.py
@@ -7,11 +7,27 @@ import scrapy
 
 
 class JacScraperItem(scrapy.Item):
+
     nazwa = scrapy.Field()
+    url = scrapy.Field()
+    data_orzeczenia = scrapy.Field()
+    prawomocnosc = scrapy.Field()
+    sad = scrapy.Field()
+    sedziowie = scrapy.Field()
+    symbol_z_opisem = scrapy.Field()
+    hasla_tematyczne = scrapy.Field()
+    poblikacja_w_uzo = scrapy.Field()
+    tresc_wyniku = scrapy.Field()
+    sentencja = scrapy.Field()
+    uzasadnienie = scrapy.Field()
+    tezy = scrapy.Field()
+    data_wplywu = scrapy.Field()
+    skarzony_organ = scrapy.Field()
+    powolane_przepisy = scrapy.Field()
+    sygn_powiazane = scrapy.Field()
 
 
-
-    # # ITEMS FOR PRACUJ.PL SCRAPER
+    # ITEMS FOR PRACUJ.PL SCRAPER
     # job_title = scrapy.Field()
     # employer = scrapy.Field()
     # price_from = scrapy.Field()
--- jac_scraper/jac_scraper/spiders/jac.py
+++ jac_scraper/jac_scraper/spiders/jac.py
@@ -3,8 +3,8 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from jac_scraper.items import JacScraperItem
 
+urls_generator = ["https://orzeczenia.nsa.gov.pl/cbo/find?p=" + str(i) for i in range(1, 2)]
 
-urls_generator = ["https://orzeczenia.nsa.gov.pl/cbo/find?p=" + str(i) for i in range(1,3)]
 
 class JacSpider(CrawlSpider):
     name = "jac"
@@ -12,34 +12,111 @@ class JacSpider(CrawlSpider):
     start_urls = urls_generator
 
     rules = (
-        Rule(LinkExtractor(allow=(r"orzeczenia.nsa.gov.pl/doc/.*",)), callback="parse_item"),
+        Rule(LinkExtractor(allow=(r"orzeczenia.nsa.gov.pl/doc/.*",),
+                           restrict_xpaths=('//td[@class="info-list-value nk"]','//td[@class="info-list-value "]')),
+             callback="parse_item"),
     )
 
     def parse_item(self, response):
-        self.logger.info("Hi, this is a judgement details page! %s", response.url)
+        # self.logger.info("------- judgement details page: ----> %s ", response.url)
+
         item = JacScraperItem()
-        item["nazwa"] = response.xpath('//span[@class="war_header"]/text()').get()
-        # item["employer"] = response.xpath('//h2[@data-scroll-id="employer-name"]/text()').get()
-        # item["price_from"] = response.xpath('//span[@data-test="text-earningAmountValueFrom"]/text()').get()
-        # item["price_to"] = response.xpath('//span[@data-test="text-earningAmountValueTo"]/text()').get()
-        # item["price_unit"] = response.xpath('//span[@data-test="text-earningAmountUnit"]/text()').get()
+        nazwa = response.xpath('//span[@class="war_header"]/text()').get()
+        url = response.url
+
+        item["nazwa"] = nazwa
+        item["url"] = url
+
+        cechy_tabela = response.xpath('//td[@class="lista-label"]/text()').getall()
+        print(cechy_tabela)
+        # wartosci_tabela = response.xpath('//td[@class="info-list-value"]/table/tr/td/text()').getall()
+        wartosci_tabela = response.xpath('//td[@class="info-list-value"]')
+
+        def clean_data(x):
+            output = ''
+            for char in x:
+                if char == '\n' or char == '\t':
+                    pass
+                else:
+                    output += char
+            return output
+
+        data_orzeczenia = response.xpath('//td[@class="info-list-value"]/table/tr/td[1]/text()').get()
+        prawomocnosc = response.xpath('//td[@class="info-list-value"]/table/tr/td[2]/text()').get()
+
+        item["prawomocnosc"] = prawomocnosc
+
+        print("data orzeczenia", data_orzeczenia)
+        print("prawomocnosc", prawomocnosc)
+
+        for i in range(len(wartosci_tabela)):
+            ab = wartosci_tabela[i].xpath('.//text()').get()
+            wartosci_tabela[i] = clean_data(ab)
+        wartosci_tabela[0] = data_orzeczenia
+        print(wartosci_tabela)
+
+
+        cechy_tekst = response.xpath('//td[@class="info-list-label-uzasadnienie"]/div/text()').getall()
+
+        # WRONG!!!!
+        wartosci_tekst = response.xpath('//td[@class="info-list-label-uzasadnienie"]/span/p/text()').getall()
+
+        # assert len(cechy_tekst) == len(wartosci_tekst)
+        for cecha, wartosc in zip(cechy_tabela, wartosci_tabela):
+            if cecha == "Data orzeczenia":
+                item['data_orzeczenia'] = wartosc
+            elif cecha == "Data wpływu":
+                item['data_wplywu'] = wartosc
+            elif cecha == "Sąd":
+                item['sad'] = wartosc
+            elif cecha == "Sędziowie":
+                item['sedziowie'] = wartosc
+            elif cecha == "Skarżony organ":
+                item['skarzony_organ'] = wartosc
+            elif cecha == "Symbol z opisem":
+                item['symbol_z_opisem'] = wartosc
+            elif cecha == "Hasła tematyczne":
+                item['hasla_tematyczne'] = wartosc
+            elif cecha == "Publikacja w u.z.o.":
+                item['poblikacja_w_uzo'] = wartosc
+            elif cecha == "Treść wyniku":
+                item['tresc_wyniku'] = wartosc
+            elif cecha == "Powołane przepisy":
+                item['powolane_przepisy'] = wartosc
+            elif cecha == "Sygn. powiązane":
+                item['sygn_powiazane'] = wartosc
+            else:
+                with open("errors.txt", "a") as file_object:
+                    file_object.write("cecha:", cecha, len(cecha))
+                    file_object.write("url:", url)
+                    file_object.write("\n")
+
+        for cecha, wartosc in zip(cechy_tekst, wartosci_tekst):
+            if cecha == "Sentencja":
+                # item['sentencja'] = wartosc
+                pass
+            elif cecha == "Uzasadnienie":
+                item['uzasadnienie'] = wartosc
+            elif cecha == "Tezy":
+                item['tezy'] = wartosc
+            else:
+                with open("errors.txt", "a") as file_object:
+                    file_object.write(cecha)
+                    file_object.write("\n")
 
         return item
 
-
-
-
-
-
-# CLASS FOR SCRAPING PRACUJ.PL JOB OFFERS
-# urls_generator = ["https://it.pracuj.pl/praca?pn=" + str(i) for i in range(1,3)]
+#
+#
+# # CLASS FOR SCRAPING PRACUJ.PL JOB OFFERS
+# pracuj_urls_generator = ["https://it.pracuj.pl/praca?pn=" + str(i) for i in range(1,2)]
 #
 #
 # class JacSpider(CrawlSpider):
 #
 #     name = "pracuj"
 #     allowed_domains = ["pracuj.pl"]
-#     start_urls = urls_generator
+#     start_urls = pracuj_urls_generator
 #
 #     rules = (
 #         Rule(LinkExtractor(allow=(r"praca/.*,oferta,.*",)), callback="parse_item"),
